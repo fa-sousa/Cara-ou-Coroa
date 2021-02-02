8bd5104caaafd186050023b15cbbdd4def208721
Fatima Ferreira de Sousa <fatimaferreiradesousa@8849-NB.local>
2021-02-02 16:10:12 -0300

    configura click de botoes

--- app/src/main/java/com/fatimasousa/caraoucoroa/MainActivity.kt
+++ app/src/main/java/com/fatimasousa/caraoucoroa/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.fatimasousa.caraoucoroa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image_jogar.setOnClickListener {
+
+            val intent = Intent(applicationContext, ResultadoActivity::class.java)
+            //gera número randomico
+            val numero = Random().nextInt(2)
+
+            //envia numero para a proxima activity
+            intent.putExtra("numero", numero)
+
+            startActivity(intent)
+
+        }
+
+
     }
 }
